fcb952c2b0121108d48fb47646432a4dfa5f14ab
George Hany <Georgehany335@gmail.com>
2020-08-14 15:41:58 +0200

    start project

---
+++ app/src/main/java/com/app/app/di/FeaturesModules.kt
@@ -0,0 +1,7 @@
+package com.app.app.di
+
+import com.feature.splash.di.splashModule
+
+var allFeatures = listOf(
+    splashModule
+)
---
+++ data/src/main/java/com/core/data/network/ApiFactory.kt
@@ -0,0 +1,6 @@
+package com.core.data.network
+
+import com.core.data.constant.AppConstant
+
+class ApiFactory(var appConstant: AppConstant) {
+}
